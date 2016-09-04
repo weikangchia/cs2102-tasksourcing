8a5f1d2c895f05cb7144b4edc21395a8a11937ad
Wei Kang <weikangchia@gmail.com>
2016-09-04 21:41:36 +0800

    Modify database configuration Change default db connection to pgsql and setup the database configuration

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -70,9 +70,9 @@ return [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => env('DB_DATABASE', 'task_db'),
             'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'P@ssw0rd'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
