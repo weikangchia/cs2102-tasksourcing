6a8a40dfbdad22c7a5fe2cff3e24727b147be2bd
Wei Kang <weikangchia@gmail.com>
2016-09-22 19:16:20 +0800

    Edit handleLogin method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\User;
 use Auth;
 
 class AuthController extends Controller
@@ -16,12 +17,13 @@ class AuthController extends Controller
 
     public function handleLogin(Request $request)
     {
+      $this->validate($request, User::$loginValidationRules);
       $data = $request->only('email', 'password');
       if(Auth::attempt($data)) {
         return redirect()->intended('home');
       }
 
-      return back()->withInput();
+      return back()->withInput()->withErrors(['email' => 'Username or password is invalid']);
     }
 
     public function logout()
