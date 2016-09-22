50beb67e81ca32bc44faa59661020579313168e7
Wei Kang <weikangchia@gmail.com>
2016-09-22 12:25:41 +0800

    Update UserTableSeeder to add more users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -5,18 +5,38 @@ use App\User;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-     public function run()
-     {
-       DB::table('users')->delete();
-       User::create([
-         'username' => 'weikangchia',
-         'email' => 'weikangchia@gmail.com',
-         'password' => bcrypt('P@ssw0rd')
-       ]);
-     }
+  /**
+  * Run the database seeds.
+  *
+  * @return void
+  */
+  public function run()
+  {
+    DB::table('users')->delete();
+
+    $users = [
+      [
+        'username' => 'weikangchia',
+        'email' => 'weikangchia@mailinator.com',
+        'password' => bcrypt('P@ssw0rd'),
+        'role' => 'user'
+      ],
+      [
+        'username' => 'david',
+        'email' => 'david@mailinator.com',
+        'password' => bcrypt('P@ssw0rd'),
+        'role' => 'user'
+      ],
+      [
+        'username' => 'admin',
+        'email' => 'admin@mailinator.com',
+        'password' => bcrypt('P@ssw0rd'),
+        'role' => 'admin'
+      ]
+    ];
+
+    foreach($users as $user){
+      User::create($user);
+    }
+  }
 }
