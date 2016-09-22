05e0e00972ff02077cea588176f2bf292cdde34a
Wei Kang <weikangchia@gmail.com>
2016-09-22 19:25:22 +0800

    Update dump file

--- database/dump.sql
+++ database/dump.sql
@@ -13,27 +13,6 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- Name: task_db; Type: COMMENT; Schema: -; Owner: forge
---
-
-COMMENT ON DATABASE task_db IS 'task database';
-
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
 SET search_path = public, pg_catalog;
 
 SET default_tablespace = '';
@@ -59,6 +38,41 @@ CREATE TABLE book (
 
 
 ALTER TABLE book OWNER TO forge;
+
+--
+-- Name: category; Type: TABLE; Schema: public; Owner: forge
+--
+
+CREATE TABLE category (
+    id integer NOT NULL,
+    name character varying(64) NOT NULL,
+    description character varying(120) NOT NULL,
+    avartar character varying(120)
+);
+
+
+ALTER TABLE category OWNER TO forge;
+
+--
+-- Name: category_id_seq; Type: SEQUENCE; Schema: public; Owner: forge
+--
+
+CREATE SEQUENCE category_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE category_id_seq OWNER TO forge;
+
+--
+-- Name: category_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: forge
+--
+
+ALTER SEQUENCE category_id_seq OWNED BY category.id;
+
 
 --
 -- Name: users; Type: TABLE; Schema: public; Owner: forge
@@ -161,6 +175,13 @@ ALTER SEQUENCE task_task_id_seq OWNED BY task.task_id;
 
 
 --
+-- Name: id; Type: DEFAULT; Schema: public; Owner: forge
+--
+
+ALTER TABLE ONLY category ALTER COLUMN id SET DEFAULT nextval('category_id_seq'::regclass);
+
+
+--
 -- Name: task_id; Type: DEFAULT; Schema: public; Owner: forge
 --
 
@@ -198,10 +219,29 @@ The New Rules of Marketing and PR: How to Use Social Media, Blogs, News Releases
 
 
 --
+-- Data for Name: category; Type: TABLE DATA; Schema: public; Owner: forge
+--
+
+COPY category (id, name, description, avartar) FROM stdin;
+3	Cleaning	House cleaning, office cleaning, spring cleaning and etc.	\N
+4	Fix Stuff	Furniture assembly, appliance repair, TV mounting and installation and etc.	\N
+2	Delivery and Removals	Furniture delivery, food Delivery, fridge moving and removal etc.	\N
+1	Everything Else	Event help, queue line up, and anything that doesn't fit into either category.	\N
+\.
+
+
+--
+-- Name: category_id_seq; Type: SEQUENCE SET; Schema: public; Owner: forge
+--
+
+SELECT pg_catalog.setval('category_id_seq', 4, true);
+
+
+--
 -- Name: member_uid_seq; Type: SEQUENCE SET; Schema: public; Owner: forge
 --
 
-SELECT pg_catalog.setval('member_uid_seq', 10, true);
+SELECT pg_catalog.setval('member_uid_seq', 37, true);
 
 
 --
@@ -235,9 +275,10 @@ SELECT pg_catalog.setval('task_task_id_seq', 4, true);
 --
 
 COPY users (id, username, email, password, first_name, last_name, role, avatar, reputation, created_at, bio, remember_token, updated_at) FROM stdin;
-8	weikangchia	weikangchia@mailinator.com	$2y$10$QNJXm3RdBDHW94vSHzYW4.RY4qCK8oAwpYJiFXk8YKjV3fbA6U3SS	\N	\N	user	\N	0	2016-09-22	\N	\N	2016-09-22
-9	david	david@mailinator.com	$2y$10$R234Bd.VgiA2TWJcm6Dky.ME0FK5EtLvM0nd6D5tG9XW/fUHvzLRy	\N	\N	user	\N	0	2016-09-22	\N	\N	2016-09-22
-10	admin	admin@mailinator.com	$2y$10$kf.h8wrPz4Omx8l0oSFeguFWToTQ6gCP.NVLPnL2F./idBFa./V5a	\N	\N	admin	\N	0	2016-09-22	\N	\N	2016-09-22
+11	weikangchia	weikangchia@mailinator.com	$2y$10$fsVRL3kQELehItkhsfKsR.cFYj10Aclh7xYYX9Lvf3DUVRPuu53CW	\N	\N	user	\N	0	2016-09-22	\N	\N	2016-09-22
+12	david	david@mailinator.com	$2y$10$2rgA.b551n8Dj4Jmh3HtfOWJgr/xsep81d.lH4l03KUu9hAHnM3Hu	\N	\N	user	\N	0	2016-09-22	\N	\N	2016-09-22
+13	admin	admin@mailinator.com	$2y$10$gMEsy4Qz3/fKJPiPNeRdUehhZL7mKjhcrMEYcEGbEei4fDSjlYF26	\N	\N	admin	\N	0	2016-09-22	\N	ICMDDR8q85dE0BOMYvcU116rIyoJvxv1G2cSnLSO6amcvAe59DFrpB9fOvU1	2016-09-22
+37	jacky	jacky@mailinator.com	$2y$10$bd7zfeucrFgQW.z41XACZe9jUXhZ8TlYQvU2pPOR5b4zStZ4nqS6C	\N	\N	user	\N	0	2016-09-22	\N	MnvzdiErFY52RnUPZaQxO4VwONH1MT8386ax5PEqYkWP7HwIxUq6s2gthFwA	2016-09-22
 \.
 
 
@@ -258,6 +299,22 @@ ALTER TABLE ONLY book
 
 
 --
+-- Name: category_name_key; Type: CONSTRAINT; Schema: public; Owner: forge
+--
+
+ALTER TABLE ONLY category
+    ADD CONSTRAINT category_name_key UNIQUE (name);
+
+
+--
+-- Name: category_pkey; Type: CONSTRAINT; Schema: public; Owner: forge
+--
+
+ALTER TABLE ONLY category
+    ADD CONSTRAINT category_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: task_pkey; Type: CONSTRAINT; Schema: public; Owner: forge
 --
 
@@ -274,14 +331,30 @@ ALTER TABLE ONLY users
 
 
 --
+-- Name: users_unique_email; Type: CONSTRAINT; Schema: public; Owner: forge
+--
+
+ALTER TABLE ONLY users
+    ADD CONSTRAINT users_unique_email UNIQUE (email);
+
+
+--
+-- Name: users_unique_username; Type: CONSTRAINT; Schema: public; Owner: forge
+--
+
+ALTER TABLE ONLY users
+    ADD CONSTRAINT users_unique_username UNIQUE (username);
+
+
+--
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
 
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
 REVOKE ALL ON SCHEMA public FROM postgres;
 GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO forge;
 GRANT ALL ON SCHEMA public TO PUBLIC;
+GRANT ALL ON SCHEMA public TO forge;
 
 
 --
