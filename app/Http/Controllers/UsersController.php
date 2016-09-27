752e5a7c82941027726e937fdcf682f0bac912f8
Wei Kang <weikangchia@gmail.com>
2016-09-27 18:54:33 +0800

    Update update method

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -88,10 +88,57 @@ class UsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+     public function update(Request $request, $id)
+     {
+       $optional = [
+         'email' => 'email|unique:users',
+         'password' => 'min:7',
+         'password_confirm' => 'same:password',
+         'first_name' => 'min:3|max:32',
+         'last_name' => 'min:3|max:32',
+         'bio' => 'max:120'
+       ];
+
+       $user = User::find($id);
+       $required = [];
+
+       if(strcmp($request->email, $user->email) != 0)
+       {
+         $required['email'] = $optional['email'];
+         $user->email = $request->email;
+       }
+
+       if($request->first_name != '')
+       {
+         $required['first_name'] = $optional['first_name'];
+         $user->first_name = $request->first_name;
+       }
+
+       if($request->last_name != '')
+       {
+         $required['last_name'] = $optional['last_name'];
+         $user->last_name = $request->last_name;
+       }
+
+       if($request->bio != '')
+       {
+         $required['bio'] = $optional['bio'];
+         $user->bio = $request->bio;
+       }
+
+       if($request->password != '')
+       {
+         $user->dirty_password = bcrypt($request->password);
+         $required['password'] = $optional['password'];
+         $required['password_confirm'] = $optional['password_confirm'];
+       }
+
+       $this->validate($request, $required);
+
+       $user->save();
+       
+       return redirect()->route('users.edit', $user->id);
+     }
 
     /**
      * Remove the specified resource from storage.
