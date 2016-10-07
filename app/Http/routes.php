c96925294546ceeb0df53236212749aa46a0afe3
Wei Kang <weikangchia@gmail.com>
2016-10-07 21:32:48 +0800

    Remove test code

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,11 +17,3 @@ Route::get('login', ['as' => 'login', 'uses' => 'AuthController@getLogin']);
 Route::post('handleLogin', ['as' => 'handleLogin', 'uses' => 'AuthController@handleLogin']);
 Route::get('logout', ['as' => 'logout', 'uses' => 'AuthController@logout']);
 Route::resource('users', 'UsersController', ['only' => ['create', 'store', 'edit', 'update']]);
-
-Route::get('books.json', function () {
-  dd(\Auth::check());
-  //$results = DB::select('SELECT * FROM book');
-  $results = DB::select('SELECT t.name, t.category FROM task t');
-
-  return response()->json($results);
-});
