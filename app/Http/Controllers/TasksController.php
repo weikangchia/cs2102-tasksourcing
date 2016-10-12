877ac1067591cbec9d5fce1a2cc6294311b62796
Wei Kang <weikangchia@gmail.com>
2016-10-12 23:54:52 +0800

    Change to fetch from database

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -4,96 +4,122 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Task;
+use Auth;
 use App\Http\Requests;
 
 define ('NEXT_LINE', '<br>');
 
 class TasksController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+	
+	
+	/**
+	* Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
     public function index()
     {
-      $task = new Task();
-      $task->name = 'Wash car 2';
-      $task->category = 'General';
-      $task->posted_by = 'Wei Kang';
-      $task->posted_by_url = '11.png';
-      $task->description = 'Date: Mon 20/10/2016'
-        .NEXT_LINE.'Time: 02:00 PM - 03:00'
-        .NEXT_LINE.'Pays: $20.00'
-        .NEXT_LINE.'Location: NUS'
-        .NEXT_LINE.NEXT_LINE.'Details:'
-        .NEXT_LINE.'I need help to wash my car on this Monday. My car will be parked at Kent Vale\'s carpark.';
-      return view('all-task')->with('task',$task);
-    }
+		if (Auth::check())
+		{
+			try {
+				$results = \DB::select(
+                    "SELECT t.id AS t_id, t.name AS task_name, t.description AS task_description,
+                        t.postal_code, t.start_date, t.start_time, t.cash_value, t.duration, t.location,
+                        c.name AS category_name, u.id AS user_id, u.username, u.profile_photo,
+                        u.reputation  FROM Task t, Category c, Users u 
+                        WHERE t.category = c.id 
+                        AND t.posted_by = u.id
+                        ORDER BY t.created_at DESC"
+                );
+			}
+			catch(QueryException $e) {
+				return false;
+			}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+			return view('all-task')->with('tasks',$results);
+		}
+		else
+        {
+			return redirect()->route('login');
+		}
+	}
+	
+	
+	
+	/**
+	* Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
     public function create()
     {
         //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+	}
+	
+	
+	
+	/**
+	* Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
     public function store(Request $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+		//
+	}
+	
+	
+	
+	/**
+	* Display the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+		//
+	}
+	
+	
+	
+	/**
+	* Show the form for editing the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
     public function edit($id)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+		//
+	}
+	
+	
+	
+	/**
+	* Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
     public function update(Request $request, $id)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+		//
+	}
+	
+	
+	
+	/**
+	* Remove the specified resource from storage.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
     public function destroy($id)
     {
-        //
-    }
+		//
+	}
 }
