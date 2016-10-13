979fefafc7a90ce2c85e7733c5fa041dbf35d416
Wei Kang <weikangchia@gmail.com>
2016-10-13 23:10:54 +0800

    Add find method

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    public static function find($key)
+    {
+        try {
+        $result = \DB::select("SELECT t.id AS t_id, t.name AS task_name, t.description AS task_description,
+                        t.postal_code, t.start_date, t.start_time, t.cash_value, t.duration, t.location,
+                        c.name AS category_name, u.id AS user_id, u.username, u.profile_photo,
+                        u.reputation
+                        FROM Task t, Category c, Users u 
+                        WHERE t.category = c.id 
+                        AND t.posted_by = u.id
+                        AND t.id = :id",
+        [
+            'id' => $key,
+        ]);
+        } catch(QueryException $e) {
+            return false;
+        }
+
+        return $result;
+    }
 }
