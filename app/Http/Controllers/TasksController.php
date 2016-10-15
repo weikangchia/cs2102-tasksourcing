8ece6d6a3ee0604548ea966e6a1223c8724e61f9
Wei Kang <weikangchia@gmail.com>
2016-10-15 13:27:45 +0800

    Add new create page

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -54,7 +54,7 @@ class TasksController extends Controller
     */
     public function create()
     {
-        //
+        return view('create-task');
 	}
 	
 	
---
+++ resources/views/create-task.blade.php
@@ -0,0 +1,19 @@
+    @extends('master')
+
+    @section('title', 'Create Task')
+
+    @section('sidebar-active-item')
+    <a class="active item">Create Tasks</a>
+    @stop
+
+    @section('content')
+    <div class="ui vertical stripe segment">
+      <div class="doubling stackable ui grid container">
+        <div class="sixteen wide column">
+          <h2>Create Tasks</h2>
+          
+        </div>
+      </div>
+    </div>
+
+    @stop
